943304e67a0a06217807eaa89d600550f68268ca
SebaschaM <schaquila@autonoma.edu.pe>
2024-05-11 02:10:13 -0500

    chore: Add dotenv dependency for environment variable management

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,18 @@ import express from "express";
 import dataClientRoute from "./routes/dataClientRoute";
 import connection from "./db/connection";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
-//process.loadEnvFile();
 connection;
+
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/client", dataClientRoute);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${3000} 🚀`);
 });
